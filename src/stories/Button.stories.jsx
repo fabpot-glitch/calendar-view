7e9922538d1455a4a6173d2bdde2aabf43489a1c
Your Name <your@email.com>
2025-11-03 22:01:23 +0530

    Remove Storybook configuration and stories folder

--- src/stories/Button.stories.jsx
+++
@@ -1,9 +0,0 @@
-// src/stories/Button.stories.jsx
-import React from "react";
-
-export const Primary = () => <button style={{ padding: "10px 20px" }}>Click me</button>;
-
-export default {
-  title: "Example/Button",
-  component: Primary,
-};
